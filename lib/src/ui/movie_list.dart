37b06f41862c5d44b273944205a475ca5b79c5b2
hexaludira <hexaludira@gmail.com>
2021-01-29 15:17:51 +0700

    Movie Detail

--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -1,3 +1,4 @@
+import 'package:blocApp/src/ui/movie_detail.dart';
 import 'package:flutter/material.dart';
 import '../models/item_model.dart';
 import '../blocs/movies_bloc.dart';
@@ -23,7 +24,7 @@ class _MovieListState extends State<MovieList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("data"),
+        title: Text("Popular Movies"),
       ),
       body: StreamBuilder(
         stream: bloc.allMovies,
@@ -45,9 +46,26 @@ class _MovieListState extends State<MovieList> {
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), 
       itemBuilder: (BuildContext context, int index) {
         return GridTile(
-          child: Image.network("https://image.tmdb.org/t/p/w185${snapshot.data.results[index].poster_path}", fit: BoxFit.cover,),
+          child: InkResponse(
+            enableFeedback: true,
+            child: Image.network("https://image.tmdb.org/t/p/w185${snapshot.data.results[index].poster_path}", fit: BoxFit.cover,),
+            onTap: () => openDetailPage(snapshot.data, index),
+          ),
         );
       }
     );
+  }
+
+  openDetailPage (ItemModel data, int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return MovieDetail(
+          title: data.results[index].title,
+          posterUrl: data.results[index].backdrop_path,
+          description: data.results[index].,
+        );
+      })
+    )
   }
 }
